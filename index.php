f652821602c59196f91f8f10201ee5aeb59ea476
Victoire Lefebvre <guylaine.a@wanadoo.fr>
2020-05-05 16:46:52 +0200

    ex2 partie1 php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$lastname = 'text'; //déclaration variable
+$firstname = 'text2'; //variable objet et message
+$age = 'text3';
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8"/>
+  <title>ex1 partie1 php</title>
+</head>
+<body>
+  <?php
+  echo 'Dupont, '; //initialiser la valeur texte
+  echo 'Claude, ';
+  echo '35ans';
+  ?>
+</body>
+</html>
